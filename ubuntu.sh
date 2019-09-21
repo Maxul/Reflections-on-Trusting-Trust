7965b9d32d3ed98442918124f7e021ea24168e02
Maxul <lmy2010lmy@gmail.com>
2019-09-21 23:38:12 +0800

    fix on ubuntu

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,30 +1,42 @@
 # make sure toolchain is available
 sudo apt-get install binutils gcc clang
 
+# prepare
+mkdir gcc-tcc clang-tcc gcc-tcc-tcc clang-tcc-tcc
+
 # obtain TCC
 wget http://download.savannah.gnu.org/releases/tinycc/tcc-0.9.27.tar.bz2
 tar -xvjf tcc-0.9.27.tar.bz2
 cd tcc-0.9.27
 
+# compile the default and install
+./configure && make -j$(nproc) && sudo make install
+
 # compile with GCC
-./configure --cc=gcc --prefix=/tmp/gcc-tcc && make -j$(nproc) && make install
+./configure --cc=gcc && make -j$(nproc)
+mv tcc ../gcc-tcc
 make clean && make distclean
 
 # compile with Clang
-./configure --cc=clang --prefix=/tmp/clang-tcc && make -j$(nproc) && make install
+./configure --cc=clang && make -j$(nproc)
+mv tcc ../clang-tcc
 make clean && make distclean
 
 # compile with GCC-TCC
-./configure --cc=/tmp/gcc-tcc/bin/tcc --prefix=/tmp/gcc-tcc-tcc && make -j$(nproc) && make install
+./configure --cc=../gcc-tcc/tcc && make -j$(nproc)
+mv tcc ../gcc-tcc-tcc
 make clean && make distclean
 
 # compile with Clang-TCC
-./configure --cc=/tmp/clang-tcc/bin/tcc --prefix=/tmp/clang-tcc-tcc && make -j$(nproc) && make install
+./configure --cc=../clang-tcc/tcc && make -j$(nproc)
+mv tcc ../clang-tcc-tcc
 make clean && make distclean
 
 cd ..
 
-# Generate SHA256 digests
-openssl dgst -sha256 /tmp/gcc-tcc-tcc/*
-openssl dgst -sha256 /tmp/clang-tcc-tcc/*
+# uninstall the default
+cd tcc-0.9.27
+./configure && make -j$(nproc) && sudo make uninstall
 
+# Generate SHA256 digests
+openssl dgst -sha256 `find . -name "tcc"`
